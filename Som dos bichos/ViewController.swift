fe36808383e7f1b70a7149694fd9dcffe8fd89e4
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-30 11:10:40 -0200

    executando varios sons

--- Som dos bichos/ViewController.swift
+++ Som dos bichos/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    
+    
+    @IBAction func cao(_ sender: Any) {
+        self.executarSom(nomeSom: "cao")
+    }
+    
+    @IBAction func gato(_ sender: Any) {
+        self.executarSom(nomeSom: "gato")
+    }
+    
+    @IBAction func leao(_ sender: Any) {
+        self.executarSom(nomeSom: "leao")
+    }
+    
+    @IBAction func macaco(_ sender: Any) {
+        self.executarSom(nomeSom: "macaco")
+    }
+    
+    @IBAction func ovelha(_ sender: Any) {
+        self.executarSom(nomeSom: "ovelha")
+    }
+    
+    @IBAction func vaca(_ sender: Any) {
+        self.executarSom(nomeSom: "vaca")
+    }
+    
+    func executarSom(nomeSom: String){
+        
+        if let path = Bundle.main.path(forResource: nomeSom, ofType: "mp3"){
+            let url = URL(fileURLWithPath: path)
+            
+            do{
+                player = try AVAudioPlayer(contentsOf: url)
+                player.prepareToPlay()
+                player.play()
+            }catch{
+                print("Erro ao executar o adudio")
+            }
+            
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
